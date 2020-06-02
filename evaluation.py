75a26dfee05f9ae5fbbeb8dcc88d4bc5369f1c02
tushar malkar <tusharjmalkar@gmail.com>
2020-06-02 20:30:50 +0530

    if and else condition

---
+++ evaluation.py
@@ -0,0 +1,6 @@
+nativevlan=input ("what is your native vlan?")
+datavlan=input("what is you data vlan?")
+if nativevlan==datavlan:
+    print("looks good")
+else:
+    print("not good pratice bhai")
